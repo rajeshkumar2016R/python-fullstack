3a343357bf9fba65acc5da44f6cd3a56d00171eb
Rajesh kumar ojha <128477624+rajeshkumar2016R@users.noreply.github.com>
2024-07-05 11:33:00 +0530

    Add files via upload

---
+++ PYTHON FULL STACK/PYTHON TUTORIAL/FIRST.py
@@ -0,0 +1,17 @@
+# print("hello world")
+
+# import calendar as cal
+# print(cal.calendar(2024))
+
+# import math
+# print(math.pi)
+# print(math.pi)
+# print(math.floor(pow(9,3)))
+# print(math.sqrt(9))
+
+import random
+
+random_number = random.randint(10, 20)
+print(random_number)
+
+
